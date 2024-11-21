eb5c29708c114349073e2aea0d8f53c322b1c790
isabelarmstrong <isabelarmstrong@mail.weber.edu>
2024-11-21 14:14:55 -0700

    Added button to display content.

--- src/components/EarlyWebSection.jsx
+++ src/components/EarlyWebSection.jsx
@@ -1,14 +1,33 @@
 import classes from "../assets/flatdesign.module.css";
 import ScopedWrapper from "../utils/ScopedWrapper";
 import ParallaxSection from "./ParallaxSection";
+import React, {useState} from "react";
 
 const EarlyWebSection = () => {
+  const [isContentVisible, setIsContentVisible] = useState(false);
+
+  const toggleContentVisibility = () => {
+    setIsContentVisible(!isContentVisible);
+  };
+
   return (
     <ScopedWrapper classes={classes}>
       <ParallaxSection date="1990s" bgColor="#ffffff" timelineColor="#000000">
         <div id="early-web" className="container py-4">
           <h1>Early Web</h1>
-          <button>Button</button>
+          <button className="btn btn-primary" onClick={toggleContentVisibility}>
+            {isContentVisible ? "Hide content" : "Show Content"}
+          </button>
+
+          {isContentVisible && (
+            <div className="content mt-4">
+              <h1>EarlyWeb</h1>
+              <p>
+                Dark mode is a theme that uses darker colors to reduce strain on the eyes,
+                especially in low-light conditions.
+              </p>
+            </div>
+          )}
         </div>
       </ParallaxSection>
     </ScopedWrapper>
